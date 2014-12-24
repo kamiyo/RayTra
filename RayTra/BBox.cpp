c720bfdbc503ff07e45e35db0973323ffebb5fca
kamiyo <kamiyo@gmail.com>
2014-12-24 01:29:09 -0500

    edited BBox

--- RayTra/BBox.cpp
+++ RayTra/BBox.cpp
@@ -16,16 +16,16 @@ vec		b[2]	(minX,minY,minZ) and (maxX,maxY,maxZ) of bounding box
 Intialize to inf
 */
 BBox::BBox() {
-	MIN.setZero(); 
-	MAX.setZero(); 
-	MAX << nINF, nINF, nINF;
-	MIN << INF, INF, INF;
+	min().setZero(); 
+	max().setZero(); 
+	max() << nINF, nINF, nINF;
+	min() << INF, INF, INF;
 	_m.setZero();
 }
 
-BBox::BBox(Vector3d min, Vector3d max) {
-	MIN.setZero(); MAX.setZero();
-	set(min, max);
+BBox::BBox(Vector3d in_min, Vector3d in_max) {
+	min().setZero(); max().setZero();
+	set(in_min, in_max);
 }
 
 BBox::~BBox() {
@@ -42,15 +42,15 @@ bool BBox::hitbox(RayBase& ray, const double t0, const double t1) const {
 	Vector3d i = ray.m_inv;
 	Vector3i s = ray.m_sign;
 
-	tmin = (b[s[0]][0] - e[0]) * i[0];
-	tmax = (b[1 - s[0]][0] - e[0]) * i[0];
-	tymin = (b[s[1]][1] - e[1]) * i[1];
-	tymax = (b[1 - s[1]][1] - e[1]) * i[1];
+	tmin = (_b[s[0]][0] - e[0]) * i[0];
+	tmax = (_b[1 - s[0]][0] - e[0]) * i[0];
+	tymin = (_b[s[1]][1] - e[1]) * i[1];
+	tymax = (_b[1 - s[1]][1] - e[1]) * i[1];
 	if ((tmin > tymax) || (tymin > tmax)) return false;
 	if (tymin > tmin) tmin = tymin;
 	if (tymax < tmax) tmax = tymax;
-	tzmin = (b[s[2]][2] - e[2]) * i[2];
-	tzmax = (b[1 - s[2]][2] - e[2]) * i[2];
+	tzmin = (_b[s[2]][2] - e[2]) * i[2];
+	tzmax = (_b[1 - s[2]][2] - e[2]) * i[2];
 	if ((tmin > tzmax) || (tzmin > tmax)) return false;
 	if (tzmin > tmin) tmin = tzmin;
 	if (tzmax < tmax) tmax = tzmax;
@@ -59,126 +59,134 @@ bool BBox::hitbox(RayBase& ray, const double t0, const double t1) const {
 
 void BBox::render(std::vector<std::vector<float> >& verts) const {
 	std::vector<float> temp;
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) min()(2));
 
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) min()(2));
 
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) min()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) min()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) min()(2));
 
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) min()(2));
 
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) min()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MAX(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) max()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MIN(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) min()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MAX(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) max()(2));
 
-	temp.push_back((float) MAX(0));
-	temp.push_back((float) MIN(1));
-	temp.push_back((float) MIN(2));
+	temp.push_back((float) max()(0));
+	temp.push_back((float) min()(1));
+	temp.push_back((float) min()(2));
 
 	verts.push_back(temp);
 }
 
-
-Vector3d BBox::min() {
-	return MIN;
+BBox BBox::combine(const BBox& b1, const BBox& b2) {
+	BBox temp_b(b1.min().cwiseMin(b2.min()), b1.max().cwiseMax(b2.max()));
+	return temp_b;
 }
 
-Vector3d BBox::max() {
-	return MAX;
+void BBox::combine(const BBox& rhs) {
+	set(min().cwiseMin(rhs.min()), max().cwiseMax(rhs.max()));
 }
 
-BBox BBox::combine(BBox& b1, BBox& b2) {
-	BBox _b(b1.MIN.cwiseMin(b2.MIN), b1.MAX.cwiseMax(b2.MAX));
-	return _b;
-}
-
-BBox BBox::combine(BBox& rhs) {
-	BBox _b(MIN.cwiseMin(rhs.MIN), MAX.cwiseMax(rhs.MAX));
-	return _b;
-}
-
-BBox BBox::transform(Matrix4d& m) {
-	BBox _b;
+BBox BBox::transform(const BBox& b, const Matrix4d& m) {
+	BBox temp_b;
 	Eigen::MatrixXd temp(8, 4);
-	temp << MIN[0], MIN[1], MIN[2], 1.0
-		, MIN[0], MIN[1], MAX[2], 1.0
-		, MIN[0], MAX[1], MIN[2], 1.0
-		, MAX[0], MIN[1], MIN[2], 1.0
-		, MIN[0], MAX[1], MAX[2], 1.0
-		, MAX[0], MIN[1], MAX[2], 1.0
-		, MAX[0], MAX[1], MIN[2], 1.0
-		, MAX[0], MAX[1], MAX[2], 1.0;
+	temp << b.min()[0], b.min()[1], b.min()[2], 1.0
+		, b.min()[0], b.min()[1], b.max()[2], 1.0
+		, b.min()[0], b.max()[1], b.min()[2], 1.0
+		, b.max()[0], b.min()[1], b.min()[2], 1.0
+		, b.min()[0], b.max()[1], b.max()[2], 1.0
+		, b.max()[0], b.min()[1], b.max()[2], 1.0
+		, b.max()[0], b.max()[1], b.min()[2], 1.0
+		, b.max()[0], b.max()[1], b.max()[2], 1.0;
 	temp.transposeInPlace();
 	temp = m * temp;
 	Array4d min = temp.rowwise().minCoeff();
 	Array4d max = temp.rowwise().maxCoeff();
-	_b.set(Vector3d(min(0), min(1), min(2)), Vector3d(max(0), max(1), max(2)));
+	temp_b.set(Vector3d(min(0), min(1), min(2)), Vector3d(max(0), max(1), max(2)));
 
-	return _b;
+	return temp_b;
 }
 
+void BBox::transform(const Matrix4d& m) {
+	Eigen::MatrixXd temp(8, 4);
+	temp <<min()[0],min()[1],min()[2], 1.0
+		,min()[0],min()[1],max()[2], 1.0
+		,min()[0],max()[1],min()[2], 1.0
+		,max()[0],min()[1],min()[2], 1.0
+		,min()[0],max()[1],max()[2], 1.0
+		,max()[0],min()[1],max()[2], 1.0
+		,max()[0],max()[1],min()[2], 1.0
+		,max()[0],max()[1],max()[2], 1.0;
+	temp.transposeInPlace();
+	temp = m * temp;
+	Array4d min = temp.rowwise().minCoeff();
+	Array4d max = temp.rowwise().maxCoeff();
+    set(Vector3d(min(0), min(1), min(2)), Vector3d(max(0), max(1), max(2)));
+}
+
+
 // BBox::set also calculates midpoint
-void BBox::set(Vector3d min, Vector3d max) {
-	MIN = min;
-	MAX = max;
-	_m = (MIN + MAX) / 2;
+void BBox::set(Vector3d in_min, Vector3d in_max) {
+	min() = in_min;
+	max() = in_max;
+	_m = (min() + max()) / 2.0;
 }
 
 bool BBox::operator==(const BBox& rhs) {
-	return ((MIN.array() == rhs.MIN.array()).all() && (MAX.array() == rhs.MAX.array()).all());
+	return ((min().array() == rhs.min().array()).all() && (max().array() == rhs.max().array()).all());
 }
 bool BBox::operator!=(const BBox& rhs) {
-	return ((MIN.array() != rhs.MIN.array()).all() && (MAX.array() != rhs.MAX.array()).all());
+	return ((min().array() != rhs.min().array()).all() && (max().array() != rhs.max().array()).all());
 }
 
 std::ostream &operator<<(std::ostream &os, BBox &b) {
-	return os << "<" << b.MIN << "> <" << b.MAX << ">";
+	return os << "<" << b.min() << "> <" << b.max() << ">";
 }
--- RayTra/BBox.h
+++ RayTra/BBox.h
@@ -11,31 +11,27 @@
 #include "UtilDefines.h"
 #include "RayBase.h"
 
-#ifndef MAX
-#define MAX b[1]
-#endif
-
-#ifndef MIN
-#define MIN b[0]
-#endif
-
-
 class BBox {
 public:
 	BBox();
 	BBox(Vector3d min, Vector3d max);
 	virtual ~BBox();
-	BBox transform(Matrix4d& m);
-	BBox combine(BBox& rhs);
-	BBox combine(BBox& b1, BBox& b2);
+	static BBox transform(const BBox& b, const Matrix4d& m);
+	void transform(const Matrix4d& m);
+	void combine(const BBox& rhs);
+	static BBox combine(const BBox& b1, const BBox& b2);
 	bool hitbox(RayBase& ray, const double t0, const double t1) const;
 	void render(std::vector<std::vector<float> >& verts) const;
-	Vector3d max();
-	Vector3d min();
+	Vector3d max() const { return _b[1]; }
+	Vector3d min() const { return _b[0]; }
+	Vector3d& max() { return _b[1]; }
+	Vector3d& min() { return _b[0]; }
+	double max(int i) const { return _b[1](i); }
+	double min(int i) const { return _b[0](i); }
 	void set(Vector3d min, Vector3d max);
 	bool operator==(const BBox& rhs);
 	bool operator!=(const BBox& rhs);
-	Vector3d b[2];
+	Vector3d _b[2];
 	Vector3d _m;
 };
 
